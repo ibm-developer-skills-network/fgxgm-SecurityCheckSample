f3ac93f53ab90c17ccda4ac4fa00b33d3da8296f
lavskillup <73978832+lavskillup@users.noreply.github.com>
2023-08-18 18:34:40 +1000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,29 +1,19 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const path = require('path');
-const PORT = process.env.PORT || 8181;
+const express = require("express")
+// const helmet = require("helmet")
 
+// creating an Express instance
+const app = express()
+const PORT = process.env.PORT || 8090
 
-// Middleware
-app.use(express.json());
-app.use(cors());
+// app.use(helmet());
 
-app.get('/', (req, res) => {
-  res.send("Hello there!");
+// initializing a basic API that
+// returns the "Hello, World!" message
+app.get("/", (req, res) => {
+    res.json("Hello, World!")
 })
 
-app.get('/gift', (req, res) => {
-  let item_queried = req.query["item"];
-  switch (item_queried){
-    case "books": res.send({'books':['Archies','Ninjago','Alex Rider']});
-    case "toys": res.send({'toys':['cars','lego blocks','nerf guns']});
-    case "clothes": res.send({'clothes':['tshrits','jeans','trackpants']});
-    default: res.send({"Unknown":item_queried})
-    }
-});
-
-  // Start the server
+// running the server
 app.listen(PORT, () => {
-console.log(`Server is running on port http://localhost:${PORT}`);
-});
+    console.log(`Starting Express server on http://localhost:${PORT}`)
+})
